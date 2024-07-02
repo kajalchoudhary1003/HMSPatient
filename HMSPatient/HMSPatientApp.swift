e7b4f4c366799cade0ac42196a57580c81d9edba
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2024-07-02 13:12:29 +0530

    Initial Commit

---
+++ HMSPatient/HMSPatientApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMSPatientApp.swift
+//  HMSPatient
+//
+//  Created by Kajal Choudhary on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HMSPatientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
