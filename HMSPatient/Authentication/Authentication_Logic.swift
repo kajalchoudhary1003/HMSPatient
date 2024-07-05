752acb47947fd122b09c4d18338278ee0d13e0c9
Aditya Kumar Gupta <connect.aadi@proton.me>
2024-07-05 14:56:26 +0530

    Trying

---
+++ HMSPatient/Authentication/Authentication_Logic.swift
@@ -0,0 +1,18 @@
+//
+//  Authentication_Logic.swift
+//  HMSPatient
+//
+//  Created by Aditya Gupta on 05/07/24.
+//
+
+import SwiftUI
+
+struct Authentication_Logic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Authentication_Logic()
+}
