8cec6deee76291064072f66f37a90ef3f2810105
Shekhar Patel <90516956+duplixx@users.noreply.github.com>
2024-07-12 23:31:12 +0530

    minor data model added

--- HMSPatient/Data Model.swift
+++ HMSPatient/Data Model.swift
@@ -141,37 +141,127 @@ struct Doctor: Codable, Identifiable, Equatable {
 }
 
 enum DoctorDesignation: String, Codable, CaseIterable {
-    case generalPractitioner = "General Practitioner"
-    case pediatrician = "Pediatrician"
-    case cardiologist = "Cardiologist"
-    case dermatologist = "Dermatologist"
-    
-    // Returns the title of the designation
-    var title: String {
-        return self.rawValue
-    }
+        case generalPractitioner = "General Practitioner"
+        case pediatrician = "Pediatrician"
+        case cardiologist = "Cardiologist"
+        case dermatologist = "Dermatologist"
+        case neurologist = "Neurologist"
+        case orthopedist = "Orthopedist"
+        case gastroenterologist = "Gastroenterologist"
+        case endocrinologist = "Endocrinologist"
+        case oncologist = "Oncologist"
+        case ophthalmologist = "Ophthalmologist"
+        case otolaryngologist = "Otolaryngologist"
+        case psychiatrist = "Psychiatrist"
+        case rheumatologist = "Rheumatologist"
+        case urologist = "Urologist"
+        case nephrologist = "Nephrologist"
+        case pulmonologist = "Pulmonologist"
+        case hematologist = "Hematologist"
+        case immunologist = "Immunologist"
+        case infectiousDiseaseSpecialist = "Infectious Disease Specialist"
+        case geriatrician = "Geriatrician"
+        case allergist = "Allergist"
+        case anesthesiologist = "Anesthesiologist"
+        case plasticSurgeon = "Plastic Surgeon"
+        case radiologist = "Radiologist"
 
-    // Returns the fees associated with the designation
-    var fees: String {
-        switch self {
-        case .generalPractitioner: return "$100"
-        case .pediatrician: return "$120"
-        case .cardiologist: return "$150"
-        case .dermatologist: return "$130"
+        // Returns the title of the designation
+        var title: String {
+            return self.rawValue
         }
-    }
 
-    // Returns the consultation interval associated with the designation
-    var interval: String {
-        switch self {
-        case .generalPractitioner: return "30"
-        case .pediatrician: return "10"
-        case .cardiologist: return "15"
-        case .dermatologist: return "20"
+        // Returns the fees associated with the designation
+        var fees: String {
+            switch self {
+            case .generalPractitioner: return "$100"
+            case .pediatrician: return "$120"
+            case .cardiologist: return "$150"
+            case .dermatologist: return "$130"
+            case .neurologist: return "$160"
+            case .orthopedist: return "$140"
+            case .gastroenterologist: return "$145"
+            case .endocrinologist: return "$150"
+            case .oncologist: return "$170"
+            case .ophthalmologist: return "$135"
+            case .otolaryngologist: return "$140"
+            case .psychiatrist: return "$155"
+            case .rheumatologist: return "$150"
+            case .urologist: return "$145"
+            case .nephrologist: return "$150"
+            case .pulmonologist: return "$150"
+            case .hematologist: return "$150"
+            case .immunologist: return "$140"
+            case .infectiousDiseaseSpecialist: return "$150"
+            case .geriatrician: return "$130"
+            case .allergist: return "$130"
+            case .anesthesiologist: return "$180"
+            case .plasticSurgeon: return "$200"
+            case .radiologist: return "$150"
+            }
         }
-    }
+        
+        // Returns the diseases treated by the designation
+        var relatedDiseases: [String] {
+            switch self {
+            case .generalPractitioner: return ["fever", "cold", "flu"]
+            case .pediatrician: return ["childhood illnesses", "growth disorders"]
+            case .cardiologist: return ["heart disease", "hypertension"]
+            case .dermatologist: return ["skin conditions", "acne"]
+            case .neurologist: return ["migraines", "seizures", "neuropathy"]
+            case .orthopedist: return ["fractures", "arthritis", "sports injuries"]
+            case .gastroenterologist: return ["IBS", "ulcers", "Crohn's disease"]
+            case .endocrinologist: return ["diabetes", "thyroid disorders", "hormonal imbalances"]
+            case .oncologist: return ["cancer", "tumors", "leukemia"]
+            case .ophthalmologist: return ["glaucoma", "cataracts", "vision problems"]
+            case .otolaryngologist: return ["sinusitis", "hearing loss", "tonsillitis"]
+            case .psychiatrist: return ["depression", "anxiety", "bipolar disorder"]
+            case .rheumatologist: return ["arthritis", "lupus", "fibromyalgia"]
+            case .urologist: return ["UTIs", "kidney stones", "prostate issues"]
+            case .nephrologist: return ["kidney disease", "hypertension", "electrolyte disorders"]
+            case .pulmonologist: return ["asthma", "COPD", "lung cancer"]
+            case .hematologist: return ["anemia", "hemophilia", "blood cancers"]
+            case .immunologist: return ["allergies", "autoimmune diseases", "immune deficiencies"]
+            case .infectiousDiseaseSpecialist: return ["HIV/AIDS", "tuberculosis", "malaria"]
+            case .geriatrician: return ["dementia", "osteoporosis", "elderly care"]
+            case .allergist: return ["allergies", "asthma", "eczema"]
+            case .anesthesiologist: return ["pain management", "anesthesia"]
+            case .plasticSurgeon: return ["reconstructive surgery", "cosmetic surgery", "burn treatment"]
+            case .radiologist: return ["diagnostic imaging", "radiation therapy"]
+            }
+        }
 
+        // Returns the consultation interval associated with the designation
+        var interval: String {
+            switch self {
+            case .generalPractitioner: return "30"
+            case .pediatrician: return "10"
+            case .cardiologist: return "15"
+            case .dermatologist: return "20"
+            case .neurologist: return "25"
+            case .orthopedist: return "20"
+            case .gastroenterologist: return "25"
+            case .endocrinologist: return "25"
+            case .oncologist: return "30"
+            case .ophthalmologist: return "20"
+            case .otolaryngologist: return "20"
+            case .psychiatrist: return "30"
+            case .rheumatologist: return "25"
+            case .urologist: return "25"
+            case .nephrologist: return "25"
+            case .pulmonologist: return "25"
+            case .hematologist: return "25"
+            case .immunologist: return "20"
+            case .infectiousDiseaseSpecialist: return "25"
+            case .geriatrician: return "20"
+            case .allergist: return "20"
+            case .anesthesiologist: return "30"
+            case .plasticSurgeon: return "40"
+            case .radiologist: return "30"
+            }
+        }
     static var withSelectOption: [DoctorDesignation?] {
         return [nil] + DoctorDesignation.allCases
     }
 }
+
