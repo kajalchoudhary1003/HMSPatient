e1397f35b470fec147d951037eae6f55d20f6a9f
Ansh Kalra <staransh24@gmail.com>
2024-07-18 02:54:03 +0530

    ye thoda sa reh gya tha

--- HMSPatient/Authentication/OtpScreen.swift
+++ HMSPatient/Authentication/OtpScreen.swift
@@ -32,7 +32,8 @@ struct OtpView: View {
                         .frame(width: 50, height: 50)
                         .overlay(
                             RoundedRectangle(cornerRadius: 10)
-                                .stroke(Color.gray, lineWidth: 1)
+                                .stroke(Color.gray, lineWidth: 2)
+                                .fill(.white)
                         )
                         .focused($focusedField, equals: index)
                         .onChange(of: otpFields[index]) { newValue in
